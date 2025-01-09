75b6a612df0d17a89bea5fd5d99d55f88c685159
mattyso <matteobrouillette@gmail.com>
2025-01-09 09:47:25 -0800

    Designing

--- src/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-    for (let elm of document.getElementsByTagName("button")) {
-        elm.addEventListener("click", (ev) => {
-            console.log("crack "+elm.id);
-        });
-    }
-});
